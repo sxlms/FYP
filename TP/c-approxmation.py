c87dab207fb0c5c2654dbc76bc5fe93e5ad7e37d
Jie <15253292176@163.com>
2022-03-01 21:26:39 +0000

    compare with h to get terminate condition of the loop line 30-31

--- TP/c-approxmation.py
+++ TP/c-approxmation.py
@@ -16,20 +16,22 @@ data = np.full((len(nodes_name), len(nodes_name)), fill_value=sys.maxsize)
 np.fill_diagonal(data, 0)
 reachability_df = pd.DataFrame(data=data, columns=nodes_name, index=nodes_name)
 # start the algorithms(advanced)
-# for i in graph_df.index:
-#     # first for u->v, u,v, t
-#     u = graph_df['i'][i]
-#     v = graph_df['j'][i]
-#     t = graph_df['t'][i]
-#     update_index = reachability_df[(reachability_df[u] < t) & (t < reachability_df[v])].index.tolist()
-#     reachability_df.loc[reachability_df.index.isin(update_index), v] = t
-#     # second for v->u, v, u, t
-#     update_index = reachability_df[(reachability_df[v] < t) & (t < reachability_df[u])].index.tolist()
-#     reachability_df.loc[reachability_df.index.isin(update_index), u] = t
+for i in graph_df.index:
+    # first for u->v, u,v, t
+    u = graph_df['i'][i]
+    v = graph_df['j'][i]
+    t = graph_df['t'][i]
+    update_index = reachability_df[(reachability_df[u] < t) & (t < reachability_df[v])].index.tolist()
+    reachability_df.loc[reachability_df.index.isin(update_index), v] = t
+    # second for v->u, v, u, t
+    update_index = reachability_df[(reachability_df[v] < t) & (t < reachability_df[u])].index.tolist()
+    reachability_df.loc[reachability_df.index.isin(update_index), u] = t
 
-# locate the location of the vertex
+cols = reachability_df.columns
+reachability = reachability_df[cols].lt(sys.maxsize).sum(axis=1).max  # compare with the h
 
-# span edge of the vj and vj+1
+
+# span edge of the vj and vj+1 and delete the edge of the graph
 j = 2
 left_nodes_list = nodes_name[0:j+1]
 right_nodes_list = np.setdiff1d(nodes_name, left_nodes_list)
@@ -40,6 +42,10 @@ for i in graph_df.index:
         delete_span_edge.append((graph_df['i'][i], graph_df['j'][i]))
 for row in delete_span_edge:
     graph_df.drop(graph_df[(graph_df['i'] == row[0]) & (graph_df['j'] == row[1])].index, inplace=True)
+
+# get the subgraph from vi,...vj, with all of its edges
+
+
 
 
 
