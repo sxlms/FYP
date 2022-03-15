9b36f8252265b10556aa7a623bb31671672b8272
Jie <15253292176@163.com>
2022-03-15 23:06:05 +0000

    Dijkstra's

--- TP/Dijkstra's_Algorithm/Dijkstra's_Algorithm.py
+++ TP/Dijkstra's_Algorithm/Dijkstra's_Algorithm.py
@@ -2,57 +2,96 @@ import numpy as np
 import pandas as pd
 import sys
 
+""" This algorithm is not optimal for the minimum edge set of a subtree with the reachability h rooted at v
+    But, it can ensure that if there is a subtree with the reachability h rooted at v. The program will output that.
+"""
 
 # Import the data.csv into dataframe
 graph_df = pd.read_csv('data.csv')
-h = 3
+h = 4
 
 # Get the list of nodes name. This list can be used as the indexes of the dataframe to show the reachability clearly
 nodes_name = list(set(graph_df['i']).union(graph_df['j']))
 nodes_set = set(nodes_name)
+nodes_number = len(nodes_name)
 
-# Initialize one dataframe record the timestamp on each edge
-temporal_function_df = pd.DataFrame(data=np.full((len(nodes_name), len(nodes_name)), fill_value=sys.maxsize),
-                                    columns=nodes_name,
-                                    index=nodes_name)
+# Initialize one matrix named edge_df record the timestamp on each edge
+edge_df = pd.DataFrame(data=np.full((len(nodes_name), len(nodes_name)), fill_value=sys.maxsize),
+                       columns=nodes_name,
+                       index=nodes_name)
 for i in graph_df.index:
-    temporal_function_df[graph_df['i'][i]][graph_df['j'][i]] = graph_df['t'][i]
-    temporal_function_df[graph_df['j'][i]][graph_df['i'][i]] = graph_df['t'][i]
+    edge_df[graph_df['i'][i]][graph_df['j'][i]] = graph_df['t'][i]
+    edge_df[graph_df['j'][i]][graph_df['i'][i]] = graph_df['t'][i]
 
-# Output: Subtree
-subtree = []
-while len(nodes_set):
-    root = nodes_set.pop()  # Root node
+# Output: Subtree of vertex with reachability is h + 1, if exist
+deleted_subtree = dict()
+break_f=0
+for root in nodes_name:
+    # it will iterate each node to find an node that reachability of subtree rooted at is h+1
     max_reachability = 1  # Every vertex can reach itself. Record the current maximum number
-    unvisited_nodes = set(nodes_name)
-    delete_edges = dict()
-    timestamps_of_vertex = pd.DataFrame(data=np.full(len(nodes_name), fill_value=sys.maxsize).reshape(1, 5),
-                                        columns=nodes_name)
-    edge_number_of_vertex = pd.DataFrame(data=np.full(len(nodes_name), fill_value=sys.maxsize).reshape(1, 5),
-                                         columns=nodes_name)
+    unvisited_nodes = set(nodes_name)  # initialize the unvisited nodes
+    unvisited_nodes.remove(root)
+    current_subtree_edges = dict()  # record the subtree edges of current node(root)
 
-    tem_edge_number = 0 if edge_number_of_vertex[root][0] == sys.maxsize else edge_number_of_vertex[root][0]
-    tem_timestamp = -1 if timestamps_of_vertex[root][0] == sys.maxsize else timestamps_of_vertex[root][0]
-    neighbor = set()
-    neighbor.add(root)
-    # start loop, find the tree of maximum reachability of h+1 of root vertex
-    while len(neighbor) and max_reachability < h:
-        root = neighbor.pop()
-        for i in temporal_function_df[root].index:
-            if temporal_function_df[root][i] != sys.maxsize and temporal_function_df[root][i] > tem_timestamp:
-                # can reach i
-                if i in unvisited_nodes:
-                    neighbor.add(i)
-                if edge_number_of_vertex[i][0] < tem_edge_number + 1:
-                    break
-                else:
-                    if edge_number_of_vertex[i][0] > tem_edge_number + 1:
-                        max_reachability += 1
-                        edge_number_of_vertex[i][0] = tem_edge_number + 1
-                    timestamps_of_vertex[i][0] = temporal_function_df[root][i]
+    # record length of the shortest temporal path from root to each nodes
+    len_df = pd.DataFrame(data=np.full(len(unvisited_nodes), fill_value=sys.maxsize).reshape(1, nodes_number - 1),
+                          columns=list(unvisited_nodes))
+    # record the earliest timestamp from root to each nodes
+    time_df = pd.DataFrame(data=np.full(len(unvisited_nodes), fill_value=sys.maxsize).reshape(1, nodes_number - 1),
+                           columns=list(unvisited_nodes))
+    tem_timestamp = -1
+    tem_length = 0
+    break_w = 0
+
+    while len(unvisited_nodes) >= 0:
+        for i in edge_df[root].index:
+            # find the root's neighbor and update the len_df and edge_df
+            if i != root and edge_df[root][i] != sys.maxsize and edge_df[root][i] > tem_timestamp:
+                # this is the reachable neighbor of root
+                # update the time_df and len_df
+                change_flag = 0  # indicate if it needs update the subtree
+                if time_df[i][0] > edge_df[root][i]:
+                    time_df[i][0] = edge_df[root][i]
+                    len_df[i][0] = tem_length + 1
+                    max_reachability += 1
+                    change_flag = 1
+                elif (time_df[i][0] == edge_df[root][i]) and (len_df[i][0] > tem_length + 1):
+                    len_df[i][0] = tem_length + 1
+                    change_flag = 1
+                # update the subtree
+                if change_flag ==1:
                     path_to_node = []
-                    if temporal_function_df[root][i] in delete_edges:
-                        path_to_node = delete_edges.get(root)
+                    if i in current_subtree_edges:
+                        path_to_node = current_subtree_edges.get(i)
                     path_to_node.append((root, i))
-                    delete_edges.update({i: path_to_node})
+                    current_subtree_edges.update({i: path_to_node})
+                # check the reachability
+                if max_reachability == h+1:
+                    break_w = 1
+                    break
+
+        if break_w == 1:
+            break_f = 1
+            break
+
+        # select the next nodes
+        tem_timestamp = sys.maxsize
+        for index, row in time_df.iteritems():
+            if (index in unvisited_nodes) and row[0] < tem_timestamp:
+                root = index
+                tem_timestamp = time_df[index][0]
+                tem_length = len_df[index][0]
+        # Delete it from unvisited nodes
         unvisited_nodes.remove(root)
+    if break_f ==1:
+        deleted_subtree = current_subtree_edges
+        break
+
+# Final output
+if break_f == 1:
+    print("there such exist subtree and deleted Subtree is:")
+
+
+else:
+    print("No such subtree")
+
