13b6edc8917a98ec02ef4da19a4d65be9726997c
Jie <15253292176@163.com>
2022-03-16 15:21:26 +0000

    d

--- TP/deleting/h-approxmation.py
+++ TP/deleting/h-approxmation.py
@@ -3,7 +3,6 @@ import pandas as pd
 import sys
 
 
-
 # Import the data.csv into dataframe
 from Utility.utility import Algorithm
 
@@ -13,6 +12,29 @@ h = 4
 reachability = Algorithm.check_reachability()
 output_edge = []
 while reachability > h:
-    edge_set = Algorithm.dijkstra_subtree(graph_df, h)
+    edge_set_dict = Algorithm.dijkstra_subtree(graph_df, h)
+    for values in edge_set_dict.values():
+        output_edge.append(values[0])  # add to the deleted edge
+        # update the graph edge
+        graph_df.drop(graph_df[(graph_df.i == values[0][0] & graph_df.j == values[0][1]) |
+                               (graph_df.j == values[0][0] & graph_df.i == values[0][1])].index, inplace=True)
+
+    reachability = Algorithm.check_reachability()
+
+if len(output_edge) > 0:
+    print("delete edges are:")
+    for values in output_edge:
+        print(values)
+else:
+    print("the reachability of graph is at most h")
+
+
+
+
+
+
+
+
+
 
 
